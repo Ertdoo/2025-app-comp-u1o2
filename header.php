a952f61dd23fb5406690d37b6314fa60e433090c
Ertdoo <168409381+Ertdoo@users.noreply.github.com>
2025-05-19 17:57:12 +1000

    Updated header.php to make navbar work in all files

--- header.php
+++ header.php
@@ -1,5 +1,4 @@
-<!doctype html>
-<html lang="en" data-bs-theme="auto">
+<!doctype html><html lang="en" data-bs-theme="auto">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,36 +6,34 @@
     <meta name="author"
       content="Mark Otto, Jacob Thornton, and Bootstrap contributers">
     <meta name="generator" content="Hugo 0.145.0">
-    <title>Carousel Template</title>
+    <title>y11AppComp</title>
 
     <link rel="canonical"
       href="https://getbootstrap.com/docs/5.3/examples/carousel/">
 
-    <script src="./assets/js/color-modes.js"></script>
+    <script src="/0/school/2025yr11php/assets/js/color-modes.js"></script>
 
-    <link href="./assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/0/school/2025yr11php/assets/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/0/school/2025yr11php/css/mystyle.css" rel="stylesheet">
+    <link href="/0/school/2025yr11php/css/carousel.css" rel="stylesheet">
 
     <link rel="apple-touch-icon"
-      href="./assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-    <link rel="icon" href="./assets/img/favicons/favicon-32x32.png"
+      href="/0/school/2025yr11php/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/0/school/2025yr11php/assets/img/favicons/favicon-32x32.png"
       sizes="32x32" type="image/png">
-    <link rel="icon" href="./assets/img/favicons/favicon-16x16.png"
+    <link rel="icon" href="/0/school/2025yr11php/assets/img/favicons/favicon-16x16.png"
       sizes="16x16" type="image/png">
-    <link rel="manifest" href="./assets/img/favicons/manifest.json">
-    <link rel="mask-icon" href="./assets/img/favicons/safari-pinned-tab.svg"
+    <link rel="manifest" href="/0/school/2025yr11php/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/0/school/2025yr11php/assets/img/favicons/safari-pinned-tab.svg"
       color="#712cf9">
-    <link rel="icon" href="./assets/img/favicons/favicon.ico">
+    <link rel="icon" href="/0/school/2025yr11php/assets/img/favicons/favicon.ico">
     <meta name="theme-color" content="#712cf9">
 
     <style>
 
     </style>
 
-    <!-- Custom styles for this template -->
-    <link href="./css/mystyle.css" rel="stylesheet">
-
-    <!-- Custom styles for this template -->
-    <link href="./css/carousel.css" rel="stylesheet">
+    <script src="/0/school/2025yr11php/assets/dist/js/bootstrap.bundle.min.js"></script>
   </head>
   <body>
 
